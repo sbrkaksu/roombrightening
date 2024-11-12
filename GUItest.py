1d2ac93e9e2d37e36ecbd50fdf7bad541a6247db
unknown <Admin@ltipc29.lti.local>
2024-11-12 12:52:25 +0100

    Timer Versuch

--- GUItest.py
+++ GUItest.py
@@ -9,9 +9,11 @@ from websockets.asyncio import client as ws
 from websockets import ConnectionClosed as ExceptionConnectionClosed
 
 import requests
+import time
 
 class App(ctk.CTk, AsyncCTk):
     def __init__(self):
+        #self.root = root
         super().__init__()
 
         self.title("Studie Raumaufhellung")
@@ -22,19 +24,29 @@ class App(ctk.CTk, AsyncCTk):
 
         # Build Frame to query if the lighting scene was bothering or not
         self.inquery_frame = ctk.CTkFrame(self)
-        self.inquery_frame.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+        #self.inquery_frame.grid(row=0, column=0, padx=10, pady=10, sticky="w")
         
-        self.inquery_label = ctk.CTkLabel(self.inquery_frame, text="Ist die Szene störend?").grid(row=0, column=0, padx=10, pady=10, sticky="w")
+        self.inquery_label = ctk.CTkLabel(self, text="Ist die Szene störend?").grid(row=0, column=0, padx=10, pady=10, sticky="w")
         self.radio_var = tk.StringVar(value="")
-        self.radio_yes = ctk.CTkRadioButton(self.inquery_frame, text="Ja (J)", command=self.radio_cb, variable=self.radio_var, value="Ja")
-        self.radio_yes.grid(row=1, column=0, padx=10, pady=10, sticky="w")
-        self.radio_yes.configure(state="disabled")
-        self.radio_no = ctk.CTkRadioButton(self.inquery_frame, text="Nein (N)", command=self.radio_cb, variable=self.radio_var, value="Nein")
-        self.radio_no.grid(row=2, column=0, padx=10, pady=10, sticky="w")
-        self.radio_no.configure(state="disabled")
+        self.stoer_var = tk.StringVar(value="Nein")
+        self.status_var = tk.StringVar(value="")
+        self.reiz_size = tk.IntVar(value=3)
+        self.isi_size = tk.IntVar(value=1)
 
-        self.button_next = ctk.CTkButton(self, text="Start (↵)", command=self.button_next_first_cb)
-        self.button_next.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+
+        self.clock_label = ctk.CTkLabel(self, text="", font=("Helvetica", 30))
+        self.clock_label.grid(row=3, column=3, padx=10, pady=20)
+        self.status_label = ctk.CTkLabel(self, text="", font=("Helvetica", 30))
+        self.status_label.grid(row=3, column=3, padx=10, pady=20)
+        self.start_button = ctk.CTkButton(self, text="Start Countdown", command=lambda: self.countdown_timer(10))
+       # self.start_button = ctk.CTkButton(self, text="Start", command=self.handle_action)
+        self.start_button.grid(row=1, column=3, padx=10, pady=10, sticky="w")
+
+        self.stoer_yes = ctk.CTkButton(self, text="Jo", command=self.radio_cb)
+        self.stoer_yes.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+
+      #  self.button_next = ctk.CTkButton(self, text="Start (↵)", command=self.button_next_first_cb)
+      #  self.button_next.grid(row=0, column=1, padx=10, pady=10, sticky="w")
         
         self.qlc_queue = asyncio.Queue(maxsize = 100)
         self.button_connect_qlc = ctk.CTkButton(self, text="Connect to QLC+", command=self.connect_qlc)
@@ -53,6 +65,22 @@ class App(ctk.CTk, AsyncCTk):
         self.button_load_qlc_project = ctk.CTkButton(self, text="Load QLC+ Project", command= self.load_qlc_project)
         self.button_load_qlc_project.grid(row=3, column=1, padx=10, pady=10, sticky="w")
         
+
+    def countdown_timer(self, seconds):
+        if seconds > 0:
+            mins, secs = divmod(seconds, 60)  # Minuten und Sekunden berechnen
+            timer_format = f'{mins:02d}:{secs:02d}'
+            self.clock_label.configure(text=timer_format)  # Timer-Wert im Label anzeigen
+            self.after(1000, self.countdown_timer, seconds - 1)  # Countdown alle 1000ms aktualisieren
+        else:
+            self.clock_label.configure(text="abgelaufen!")
+
+#    def handle_action(self, status):
+
+
+
+
+
     def load_qlc_project(self):
         filename = tk.filedialog.askopenfilename(filetypes=[("QLC+ files", "*.qxw"),("All files", "*.*")])
         with open(filename, 'rb') as f:
@@ -67,6 +95,8 @@ class App(ctk.CTk, AsyncCTk):
     async def connect_qlc(self):
         # connect to QLC+ with auto-reconnect on connection closed
         async for qlcsocket in ws.connect('ws://localhost:9999/qlcplusWS'):
+            self.button_connect_qlc.configure(fg_color="green")
+            self.button_connect_qlc.configure(state="disabled")
             print("connected to QLC+")
             try:
                 while True:
@@ -94,7 +124,10 @@ class App(ctk.CTk, AsyncCTk):
             self.button_next.configure(state="normal")
         
     def radio_cb(self):
-        pass
+        print("Knopf gedrückt")
+        self.stoer_var.set("Ja")
+        print("Variabel ->", self.stoer_var.get())
+
 
 app = App()
 app.async_mainloop()
