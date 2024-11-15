cbaae13e76d09652053c053ac77d6843b6e15155
Leonhard Dudzik <Dudzik@lti.local>
2024-11-15 16:37:48 +0100

    SzenenSteuerung

--- GUItest.py
+++ GUItest.py
@@ -1,56 +1,113 @@
+import ast
+
 import tkinter as tk
 import customtkinter as ctk
 
 import asyncio
 from async_tkinter_loop import async_handler
 from async_tkinter_loop.mixins import AsyncCTk
+import async_timer
+# maybe use uvloop (fast / more time-accurate event loop)
+
+
+from CTkMenuBar import CTkTitleMenu
+from CTkTable import CTkTable
 
 from websockets.asyncio import client as ws
 from websockets import ConnectionClosed as ExceptionConnectionClosed
 
 import requests
-import time
+
+
+import numpy as np
 
 class App(ctk.CTk, AsyncCTk):
     def __init__(self):
-        #self.root = root
         super().__init__()
-
+        ######## Setup the Window ########
         self.title("Studie Raumaufhellung")
-        self.geometry("600x400")
-        self.grid_columnconfigure((0, 1), weight=1)
+        self.geometry("1200x900")
+        ##################################
+        
+        ######## Settings ########
+        self.settings = {
+            "szene_duration": 2.7, # seconds
+            "inter-stimulus-interval": 2.0 #seconds
+        }
+
+        #self.grid_columnconfigure((0, 1), weight=1)
         #self.grid_rowconfigure((0, 1), weight=1)
 
 
-        # Build Frame to query if the lighting scene was bothering or not
+        ######## Frame to simulate Input on Light Szene, bothering or not? ########
         self.inquery_frame = ctk.CTkFrame(self)
-        #self.inquery_frame.grid(row=0, column=0, padx=10, pady=10, sticky="w")
-        
-        self.inquery_label = ctk.CTkLabel(self, text="Ist die Szene störend?").grid(row=0, column=0, padx=10, pady=10, sticky="w")
-        self.radio_var = tk.StringVar(value="")
+        self.inquery_frame.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+
+        self.inquery_label = ctk.CTkLabel(self.inquery_frame, text="Ist die Szene störend?").grid(row=0, column=0, padx=10, pady=10, sticky="w")
         self.stoer_var = tk.StringVar(value="Nein")
-        self.status_var = tk.StringVar(value="")
-        self.reiz_size = tk.IntVar(value=3)
-        self.isi_size = tk.IntVar(value=1)
+        self.stoer_button = ctk.CTkButton(self.inquery_frame, text="Jo", command=lambda: self.stoer_var.set("Ja"))
+        self.stoer_button.grid(row=1, column=0, padx=10, pady=10, sticky="w")
+        ###########################################################################
+                
+        ######## Frame to display the sequences in a table ########
+        self.sequence_frame = ctk.CTkFrame(self)
+        self.sequence_frame.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+        
+        
+        self.sequence_label = ctk.CTkLabel(self.sequence_frame, text="Durchgang", anchor="nw")
+        self.sequence_label.grid(row=0, column=0, padx=0, pady=0)
+        
+        self.scene_label = ctk.CTkLabel(self, text="Szene")
+        self.scene_label.grid(row=1, column=2, padx=10, pady=10, sticky="nw")
+        
+        self.sequence_table_header = CTkTable(self.sequence_frame, row=1, column=4, header_color='white', corner_radius=0, height =12, width=60)
+        self.sequence_table_header.grid(row=1, column=0, padx=10, pady=0, sticky="n")
+        self.sequence_table_header.update_values([["Szene", "Spot", "E","Störend"]])
+        self.scene_rows = 16
+        self.sequence_table = CTkTable(self.sequence_frame, row=self.scene_rows, column=4, corner_radius=0, height =12, width=60)
+        self.sequence_table.grid(row=2, column=0, padx=10, pady=(0,10), sticky="n")
+        
+        self.sequence_progressbar = ctk.CTkProgressBar(self.sequence_frame, orientation="vertical", mode = "determinate", width = 6)
+        self.sequence_progressbar.grid(row=2, column=1, padx=0, pady=(0,1), sticky="nsw")
+        self.sequence_progressbar.configure(corner_radius=0)
+        # swap progressbar colors, so it "fills" from top to bottom
+        self.sequence_progressbar.configure(progress_color=ctk.ThemeManager.theme["CTkProgressBar"]["fg_color"])
+        self.sequence_progressbar.configure(fg_color=ctk.ThemeManager.theme["CTkProgressBar"]["progress_color"])
+        self.sequence_progressbar.set(1)
+        
+        
+        ######## Frame to control the sequence ########
+        self.seq_crtl_frame = ctk.CTkFrame(self)
+        self.seq_crtl_frame.grid(row=1, column=0, padx=10, pady=10, sticky="wn")
+        self.load_proband_button = ctk.CTkButton(self.seq_crtl_frame, text="Proband laden", command=self.load_proband)
+        self.load_proband_button.grid(row=0, column=0, padx=10, pady=10, sticky="n")
+        self.sequence_start_reset_button = ctk.CTkButton(self.seq_crtl_frame, text="Durchgang starten", command=self.run_sequence, state="disabled")
+        self.sequence_start_reset_button.grid(row=2, column=0, padx=10, pady=10, sticky="s")
+        
+        self.sequence_stop_continue_button = ctk.CTkButton(self.seq_crtl_frame, text="Durchgang anhalten", command=self.stop_sequence, state="disabled")
+        self.sequence_stop_continue_button.grid(row=3, column=0, padx=10, pady=10, sticky="s")
+        
+        self.sequence_stop_event = asyncio.Event()
+        self.sequence_continue_event = asyncio.Event()
 
-
-        self.clock_label = ctk.CTkLabel(self, text="", font=("Helvetica", 30))
-        self.clock_label.grid(row=3, column=3, padx=10, pady=20)
-        self.status_label = ctk.CTkLabel(self, text="", font=("Helvetica", 30))
-        self.status_label.grid(row=3, column=3, padx=10, pady=20)
-        self.start_button = ctk.CTkButton(self, text="Start Countdown", command=lambda: self.countdown_timer(10))
-       # self.start_button = ctk.CTkButton(self, text="Start", command=self.handle_action)
-        self.start_button.grid(row=1, column=3, padx=10, pady=10, sticky="w")
-
-        self.stoer_yes = ctk.CTkButton(self, text="Jo", command=self.radio_cb)
-        self.stoer_yes.grid(row=1, column=0, padx=10, pady=10, sticky="w")
-
-      #  self.button_next = ctk.CTkButton(self, text="Start (↵)", command=self.button_next_first_cb)
-      #  self.button_next.grid(row=0, column=1, padx=10, pady=10, sticky="w")
+        self.seq_switch_frame = ctk.CTkFrame(self.seq_crtl_frame, fg_color="transparent")
+        self.seq_switch_frame.grid(row=1, column=0, padx=0, pady=0, sticky="wn")
+        self.next_seq_button = ctk.CTkButton(self.seq_switch_frame, text=">", width=60, command=self.next_sequence)
+        self.next_seq_button.grid(row=0, column=1, padx=10, pady=10, sticky="e")
+        self.prev_seq_button = ctk.CTkButton(self.seq_switch_frame, text="<", width=60, command=self.prev_sequence)
+        self.prev_seq_button.grid(row=0, column=0, padx=10, pady=10, sticky="w")
+        
+        
+        ######## Countdown Timer ########
+        self.scene_countdown_timer =  tk.DoubleVar()
+        self.scene_countdown_timer.trace_add('write', self.print_scene_countdown)
+        self.scene_countdown_finished = asyncio.Event()
+        self.scene_countdown_label = ctk.CTkLabel(self, text="", font=("Helvetica", 30))
+        self.scene_countdown_label.grid(row=3, column=3, padx=10, pady=20)
         
         self.qlc_queue = asyncio.Queue(maxsize = 100)
-        self.button_connect_qlc = ctk.CTkButton(self, text="Connect to QLC+", command=self.connect_qlc)
-        self.button_connect_qlc.grid(row=1, column=1, padx=10, pady=10, sticky="w")
+        self.connect_qlc_button = ctk.CTkButton(self, text="Connect to QLC+", command=self.connect_qlc)
+        self.connect_qlc_button.grid(row=1, column=3, padx=10, pady=10, sticky="nw")
 
         self.dmx_channel = ctk.CTkEntry(self, placeholder_text="DMX Channel")
         self.dmx_channel.grid(row=2, column=0, padx=10, pady=10, sticky="w")
@@ -60,36 +117,157 @@ class App(ctk.CTk, AsyncCTk):
         self.button_set_dmx_channel = ctk.CTkButton(self, text="Set DMX Channel",
                                                     command= lambda: self.set_dmx_channel(int(self.dmx_channel.get()),
                                                                                           int(self.dmx_value.get())))
-        self.button_set_dmx_channel.grid(row=2, column=1, padx=10, pady=10, sticky="w")
+        self.button_set_dmx_channel.grid(row=2, column=2, padx=10, pady=10, sticky="w")
         
         self.button_load_qlc_project = ctk.CTkButton(self, text="Load QLC+ Project", command= self.load_qlc_project)
-        self.button_load_qlc_project.grid(row=3, column=1, padx=10, pady=10, sticky="w")
-        
-
-    def countdown_timer(self, seconds):
-        if seconds > 0:
-            mins, secs = divmod(seconds, 60)  # Minuten und Sekunden berechnen
-            timer_format = f'{mins:02d}:{secs:02d}'
-            self.clock_label.configure(text=timer_format)  # Timer-Wert im Label anzeigen
-            self.after(1000, self.countdown_timer, seconds - 1)  # Countdown alle 1000ms aktualisieren
-        else:
-            self.clock_label.configure(text="abgelaufen!")
-
-#    def handle_action(self, status):
-
-
-
-
+        self.button_load_qlc_project.grid(row=3, column=2, padx=10, pady=10, sticky="w")
 
     def load_qlc_project(self):
         filename = tk.filedialog.askopenfilename(filetypes=[("QLC+ files", "*.qxw"),("All files", "*.*")])
         with open(filename, 'rb') as f:
             r = requests.post('http://127.0.0.1:9999/loadProject', files={'qlcprj': f})
+    
+    def load_proband(self):
+        filename = tk.filedialog.askopenfilename(filetypes=[("Text file", "*.txt"),("All files", "*.*")])
+        with open(filename, 'r') as f:
+            s = f.read()
+            self.proband = ast.literal_eval(s)
+        self.seq_num = len(self.proband["Durchgange"])
+        self.seq_idx = 0
+        self.set_sequence()
+        self.sequence_start_reset_button.configure(state="normal")
+        
+    def next_sequence(self):
+        self.seq_idx += 1
+        if self.seq_idx >= self.seq_num:
+            self.seq_idx -= self.seq_num
+        self.set_sequence()
+    
+    def prev_sequence(self):
+        self.seq_idx -= 1
+        if self.seq_idx < 0:
+            self.seq_idx += self.seq_num
+        self.set_sequence()
+
+    def set_sequence(self):
+        if self.proband is not None:
+            self.sequence = self.proband["Durchgange"][self.seq_idx]
+        seq_values = [[s["ID"],s["Spot"],s["E"]] for s in self.sequence["Szenen"]]
+        
+        self.sequence_table.update_values(seq_values)
+        self.sequence_label.configure(text="Durchgang {id}".format(id=self.sequence["ID"]))
+    
+    
+    def stop_sequence(self):
+        self.sequence_stop_continue_button.configure(text="Durchgang fortsetzen",
+                                                  command=self.continue_sequence,
+                                                  fg_color=ctk.ThemeManager.theme["CTkButton"]["fg_color"])
+        self.sequence_stop_event.set()
+    
+
+    def continue_sequence(self):
+        self.sequence_stop_continue_button.configure(text="Durchgang anhalten",
+                                                  command=self.stop_sequence,
+                                                  fg_color="red")
+        self.sequence_continue_event.set()
+        
+    def reset_sequence(self):
+        self.sequence_task.cancel()
+    
+    def deselect_table(self):
+        for i in range(len(self.sequence["Szenen"])):
+            self.sequence_table.deselect_row(i)
+    
+    async def run_sequence_task(self):
+        try:
+            # switch button to stop sequence
+            self.sequence_stop_continue_button.configure(state='normal',fg_color="red")
+            self.sequence_start_reset_button.configure(text="Durchgang zurücksetzen", command=self.reset_sequence)
+            scene_num = len(self.sequence["Szenen"])
+            
+            for scene_idx, scene in enumerate(self.sequence["Szenen"]):
+                next_scene = False
+                while not next_scene:
+                    # set scene label
+                    self.sequence_table.select_row(scene_idx)
+                    self.scene_label.configure(text="Szene {id}/{num}".format(id=scene["ID"], num=scene_num))
+                    # highlight scene row in table
+                    # set scene DMX values
+                    print("setting scene...")
+                    # wait for scene duration
+                    
+                    await self.await_countdown_timer(self.settings["szene_duration"])
+
+                    # set inter-stimulus lighting
+                    print("setting inter-stimulus lighting...")
+                    # wait for inter-stimulus interval
+                    await self.await_countdown_timer(self.settings["inter-stimulus-interval"])
+                    if self.sequence_stop_event.is_set(): # if stopped, keep interstimulus lighting
+                        await self.sequence_continue_event.wait() # wait for continue event
+                        self.sequence_continue_event.clear()
+                        self.sequence_stop_event.clear()
+                        continue
+                    next_scene = True
+                    self.sequence_table.deselect_row(scene_idx)
+                    # evaluate if button was pressed
+                    # use a asyncio Event
+        finally:
+            self.deselect_table()
+            self.sequence_start_reset_button.configure(text="Durchgang starten", command=self.run_sequence)
+            self.sequence_stop_continue_button.configure(text="Durchgang anhalten", command=self.stop_sequence,
+                                                        state="disabled", fg_color=ctk.ThemeManager.theme["CTkButton"]["fg_color"])
+            self.sequence_stop_event.clear()
+            self.scene_label.configure(text="Szene")
+            self.scene_countdown_timer.set(0)
+            self.sequence_stop_continue_button.configure(state="disabled")
+            self.sequence_start_reset_button.configure(state="normal")
+
+    @async_handler
+    async def run_sequence(self):
+        self.sequence_task = asyncio.create_task(self.run_sequence_task(), name="run_sequence")
+        await self.sequence_task
+            
+
+    def print_scene_countdown(self, *args):
+        self.scene_countdown_label.configure(text="{:04.1f}".format(self.scene_countdown_timer.get()))
+        
+    def set_progressbar(self, *args):
+        pass
+        #self.scene_rows
+        #self.scene_countdown_timer.get()
+        #self.scene_idx
+        #self.sequence_progressbar.set(1-)
+        
+    def countdown_timer(self):
+        timer_value = self.scene_countdown_timer.get()
+        timer_value -= 0.1
+        if timer_value > 0:
+            self.scene_countdown_timer.set(np.round(timer_value,decimals=1))
+            self.after(100, self.countdown_timer)  # Countdown alle 100ms aktualisieren
+        else:
+            self.scene_countdown_finished.set()
+
+    async def await_countdown_timer(self, time):
+        self.scene_countdown_finished.clear()
+        self.scene_countdown_timer.set(time)
+        self.after(100, self.countdown_timer) # starts the timer
+        await asyncio.wait(
+            [asyncio.create_task(self.scene_countdown_finished.wait()),
+             asyncio.create_task(self.sequence_stop_event.wait())],return_when=asyncio.FIRST_COMPLETED)
+        self.scene_countdown_timer.set(0)
+        
 
     @async_handler
     async def set_dmx_channel(self,channel,value):
-        print("put in queue: ", "CH|{ch}|{v}".format(ch=channel, v=value))
         self.qlc_queue.put_nowait("CH|{ch}|{v}".format(ch=channel, v=value))
+    
+    @async_handler
+    async def set_qlc_widget(self,id,value):
+        self.qlc_queue.put_nowait("{id}|{val}".format(id=id, val=value))
+    
+    @async_handler
+    async def step_cue_list(self,id,index):
+        self.qlc_queue.put_nowait("{id}|STEP|{idx}".format(id=id, idx=index))
     
     @async_handler
     async def connect_qlc(self):
@@ -104,30 +282,50 @@ class App(ctk.CTk, AsyncCTk):
                     await qlcsocket.send(msg)
             except ExceptionConnectionClosed:
                 continue
-    
-    
-    def button_next_first_cb(self):
-        self.radio_yes.configure(state="normal")
-        self.radio_no.configure(state="normal")
-        self.radio_var.trace("w", self.radio_var_cb)
-        self.button_next.configure(text="nächste Szene (↵)")
-        self.button_next.configure(command=self.button_next_cb)
-
-    def button_next_cb(self):
-        print("scene bothering? -> ", self.radio_var.get())
-        self.radio_var.set("")
-        
-    def radio_var_cb(self, *args):
-        if self.radio_var.get() == "":
-            self.button_next.configure(state="disabled")
-        else:
-            self.button_next.configure(state="normal")
-        
-    def radio_cb(self):
-        print("Knopf gedrückt")
-        self.stoer_var.set("Ja")
-        print("Variabel ->", self.stoer_var.get())
+            
 
 
 app = App()
 app.async_mainloop()
+
+proband = {
+            "ID": 1,
+            "Durchgange":[
+                {
+                    "ID" : 1,
+                    "Matt": False,
+                    "Szenen" : [
+                        {"ID": 1, "Spot" : 1, "E" : 30},
+                        {"ID": 2, "Spot" : 2, "E" : 1},
+                        {"ID": 3, "Spot" : 1, "E" : 6},
+                        {"ID": 4, "Spot" : 2, "E" : 0.1},
+                        {"ID": 5, "Spot" : 4, "E" : 30},
+                        {"ID": 6, "Spot" : 4, "E" : 100}
+                        ]
+                },
+                {
+                    "ID" : 2,
+                    "Matt": False,
+                    "Szenen" : [
+                        {"ID": 1, "Spot" : 1, "E" : 30},
+                        {"ID": 2, "Spot" : 3, "E" : 1},
+                        {"ID": 3, "Spot" : 4, "E" : 6},
+                        {"ID": 4, "Spot" : 2, "E" : 0.3},
+                        {"ID": 5, "Spot" : 4, "E" : 10},
+                        {"ID": 6, "Spot" : 3, "E" : 60}
+                        ]
+                },
+                {
+                    "ID" : 3,
+                    "Matt": True,
+                    "Szenen" : [
+                        {"ID": 1, "Spot" : 4, "E" : 30},
+                        {"ID": 2, "Spot" : 2, "E" : 1},
+                        {"ID": 3, "Spot" : 4, "E" : 6},
+                        {"ID": 4, "Spot" : 2, "E" : 0.1},
+                        {"ID": 5, "Spot" : 4, "E" : 30},
+                        {"ID": 6, "Spot" : 1, "E" : 100}
+                        ]
+                }
+            ]
+        }
