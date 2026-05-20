dfed14893e06c5611fa9777ded338fe37fdb53c7
Berk Aksu <berkaksu79@gmail.com>
2026-05-20 17:10:18 +0200

    trial.py was created for small tests

--- trial.py
+++ trial.py
@@ -1,6 +1,18 @@
 import numpy as np
 
-rng = np.random.default_rng(1)
+num_E_steps_per_decade = 6
 
-print(rng)
-print(rng.integers(0, 10))
+E_decades_Evening = (-1, 0, 1, 2)  # 10^...
+E_decades_Night = (-2, -1, 0, 1)  # 10^...
+
+
+E_steps_evening = np.hstack(
+    [
+        np.logspace(0, 1, num=num_E_steps_per_decade + 1)[:-1] * i
+        for i in np.logspace(
+            E_decades_Evening[0], E_decades_Evening[-1], len(E_decades_Evening)
+        )
+    ]
+).tolist()[:-2]
+
+print(np.logspace(0, 1, 7)[:-1] * 10)
