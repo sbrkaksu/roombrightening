2d5e1bc23a0cd7250ae99fd24f8a7d645ebaa5e7
Berk Aksu <berkaksu79@gmail.com>
2026-06-21 16:06:41 +0200

    working on algorithm

--- stage_staircase.py
+++ stage_staircase.py
@@ -16,12 +16,14 @@ class AdaptiveStaircase:
         # Memory (state)
         self.history = []
         self.reversal_points = []
+        self.calculated_reversal_points = []
         self.response_sequence_history = []
         self.last_direction = None
         self.trial_count = 0
         self.active_step = 0.0 # Stores the current step for display
+        self.threshold = None
 
-    def _get_dynamic_step_size(self, current_value):
+    def get_dynamic_step_size(self, current_value):
 
         if self.combination_factor == 0: #if the illumination is pure direct 
             if current_value > 100.0:
@@ -48,6 +50,10 @@ class AdaptiveStaircase:
                 return current_value / 2.154  
         else:                           #combined illumination
             pass
+
+    def is_finished(self):
+        """Checks whether the target reversals or maximum trial count has been reached."""
+        return self.trial_count >= self.max_trials or len(self.reversal_points) >= self.target_reversals
     
     def update(self, response):
         """Processes the response, calculates the dynamic step, and advances the algorithm."""
@@ -59,7 +65,7 @@ class AdaptiveStaircase:
         self.response_sequence_history.append(response)
         
         # Calculate the dynamic step
-        step = self._get_dynamic_step_size(self.current_value)
+        step = self.get_dynamic_step_size(self.current_value)
         self.active_step = step
         
 
@@ -85,7 +91,7 @@ class AdaptiveStaircase:
             
         # Direction change (reversal) and final-step lock
         if self.last_direction and self.last_direction != current_direction:
-            if self.trial_count < self.max_trials:
+            if self.trial_count < self.max_trials: #do not take account the last reversal if reversal behavior is not completed       
                 self.reversal_points.append(self.history[-1])
                 is_reversal = True
                 
@@ -93,18 +99,18 @@ class AdaptiveStaircase:
         
         return is_reversal
 
-    def is_finished(self):
-        """Checks whether the target reversals or maximum trial count has been reached."""
-        return self.trial_count >= self.max_trials or len(self.reversal_points) >= self.target_reversals
-
     def get_threshold(self):
         """Calculates the average result."""
         if self.reversal_points and len(self.reversal_points) == self.target_reversals:
-            return (sum(self.reversal_points[1:]) / len(self.reversal_points[1:]))
+            self.calculated_reversal_points = self.reversal_points[1:]
+            self.threshold = sum(self.calculated_reversal_points) / len(self.calculated_reversal_points)
+            return self.calculated_reversal_points, self.threshold
         elif self.reversal_points and len(self.reversal_points) < self.target_reversals:
-            return (sum(self.reversal_points) / len(self.reversal_points))
+            self.calculated_reversal_points = self.reversal_points
+            self.threshold = sum(self.calculated_reversal_points) / len(self.calculated_reversal_points)
+            return self.calculated_reversal_points, self.threshold
         else:
-            return print("No reversals recorded, threshold calculation not possible.")
+            return "No reversals recorded, threshold calculation not possible."
 
 
     # ==========================================
@@ -148,7 +154,8 @@ class AdaptiveStaircase:
         while not self.is_finished():
         
             # Calculate the current step size for display
-            current_step = self._get_dynamic_step_size(self.current_value)
+            current_step = self.get_dynamic_step_size(self.current_value)
+            
             
             print(f"\n[Step {self.trial_count + 1}/{self.max_trials}] | Reversals: {len(self.reversal_points)}/{self.target_reversals}")
             print(f"Current stimulus intensity: {self.current_value:.2f} lx (active step size: {current_step:.4f} lx)")
@@ -168,17 +175,16 @@ class AdaptiveStaircase:
         print("  EXPERIMENT COMPLETED!")
         print("=" * 70)
 
-        if self.reversal_points:
+        if self.is_finished():
                 print("Confirmed direction changes (reversals):")
                 print(f"  {[round(x, 4) for x in self.reversal_points]} lx (Total {len(self.reversal_points)})")
                 print("\n")
                 print("Calculated reversals:")
-                print(f"  {[round(x, 4) for x in self.reversal_points[1:]]} lx (Total {len(self.reversal_points[1:])})")
+                print(f"  { [round(x, 4) for x in self.get_threshold()[0]] } lx (Total {len(self.get_threshold()[0])})")
                 print("-" * 70)
-                print(f"Calculated threshold value (reversal average): {self.get_threshold():.4f} lx")
+                print(f"Calculated threshold value (reversal average): {self.get_threshold()[1]:.4f} lx")
         else:
-                print("  No direction changes occurred.")
-                print(f"Calculated threshold value (average of the last 5 steps): {self.get_threshold():.4f} lx")
+                print(self.get_threshold())
 
         print("\nFull stimulus history:")
         print([round(x, 4) for x in self.history])
