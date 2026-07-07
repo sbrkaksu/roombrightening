5dc04e8f36c6d97c036dd284a488064634fd7f84
Berk Aksu <berkaksu79@gmail.com>
2026-07-07 18:07:02 +0200

    data types created for phases

--- Stage_ProbandGenerator.py
+++ Stage_ProbandGenerator.py
@@ -5,18 +5,41 @@ from Formatter import FormatPrinter
 
 printer = FormatPrinter({float: "{:.4e}"}, sort_dicts=False)
 
-"""
+
 #generates ProbandLernen.txt for Learning Phase
 def generate_learn_proband():
-    proband = {"ID": -1, "Abstufung": "lernen"} # Abstufung = Phase
-    durchgang = {"ID": -1, "Diffus": False, "Zeit": "Abend"}
-    durchgang["Szenen"] = rng.permutation(scenes_test).tolist()
-    proband["Durchgange"] = [durchgang]
-    with open("ProbandLernen.txt", "w") as file:
+    rng = np.random.default_rng()
+    stimuli_learning = [ 0.1, 0.147, 0.215, 0.316, 0.464, 0.681, 
+            1, 1.47, 2.15, 3.16, 4.64, 6.81, 
+            10, 14.7, 21.5, 31.6, 46.4, 68.1, 
+            100, 147, 215, 316]
+    selected_E_values = rng.choice(stimuli_learning, size=12, replace=False)
+    selected_types = rng.permutation([
+        "Direct", "Direct", "Direct", "Direct", "Direct", "Direct",
+        "Diffuse", "Diffuse", "Diffuse", "Diffuse", "Diffuse", "Diffuse",
+    ])
+    scenes = [
+        scene(
+            combination_factor=1 if scene_type == "Direct" else 0,
+            type=str(scene_type),
+            E=float(E),
+            disturbed=None,
+            reaction_time=None,
+        )
+        for scene_type, E in zip(selected_types, selected_E_values)
+    ]
+    proband = {"ID": -1, "Phase": "Learning Block", "LearnDurchgang": True, "Durchgange": [
+        {'ID': -1, "Time": "Evening", "Color": "3000 K", "Scenes" : scenes},
+    ]}
+    with open("ProbandLernenB.txt", "w") as file:
         file.write(printer.pformat(proband))
-"""
 
+
+def scene(combination_factor, type, E, disturbed, reaction_time):
+    return {"Combination_Factor": combination_factor, "Type": type, "E": E, "Disturbed": disturbed, "Reaction Time": reaction_time}
 # Generates Probandens grob when this .py file is run
+
+
 def generate_probanden_E_block():
      
         proband = {"ID": 1, "Phase": "E_Block", "LearnDurchgang": False, "Durchgange": [
@@ -31,10 +54,10 @@ def generate_probanden_E_block_results():
      
         proband = {"ID": 1, "Phase": "E_Block", "LearnDurchgang": False, "Durchgange": [
             {'ID': 1, "Time": "Evening", "Color": "3000 K", "Results" : [
-                  {"Threshold_E(lx)": None, "Reversals (lx)": [], "Reversal Points" : None},
+                  {"E_threshold": None, "Reversals (lx)": [], "Reversal Points" : None},
             ]},
             {'ID': 2, "Time": "Night", "Color": "3000 K", "Results" : [
-                  {"Threshold_E(lx)": None, "Reversals (lx)": [], "Reversal Points" : None},
+                  {"E_threshold": None, "Reversals (lx)": [], "Reversal Points" : None},
             ]},
         ]}
 
@@ -55,10 +78,10 @@ def generate_probanden_Combination_block_results():
      
         proband = {"ID": 1, "Phase": "Combination_Block", "LearnDurchgang": False, "Durchgange": [
             {'ID': 3, "Time": "Evening", "Color": "3000 K", "Results" : [
-                  {"Threshold_Combination_Factor": None, "Reversals (Combination Factor)": [], "Number of Reversals" : None},
+                  {"E": None, "Reversals (lx)": [], "Reversal Points" : None},
             ]},
             {'ID': 4, "Time": "Night", "Color": "3000 K", "Results" : [
-                  {"Threshold_Combination_Factor": None, "Reversals (Combination Factor)": [], "Number of Reversals" : None},
+                  {"E": None, "Reversals (lx)": [], "Reversal Points" : None},
             ]},
         ]}
 
@@ -66,6 +89,7 @@ def generate_probanden_Combination_block_results():
             file.write(printer.pformat(proband))
 
 if __name__ == "__main__":
+    generate_learn_proband()
     generate_probanden_E_block()
     generate_probanden_Combination_block()
     generate_probanden_E_block_results()
