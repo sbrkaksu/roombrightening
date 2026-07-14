ff3cb16b09947dadab00fb882c2b8eb2e91e4d44
Berk Aksu <berkaksu79@gmail.com>
2026-07-14 21:36:58 +0200

    reverted channel structure back

--- Stage_RaumaufhellungSteuerung.py
+++ Stage_RaumaufhellungSteuerung.py
@@ -1306,16 +1306,16 @@ class App(ctk.CTk, AsyncCTk):
 
         self.spot1_intensity    = self.qlc_input.add_channel(start=1, width=2) # Direct spot 1
         self.diffuse_intensity  = self.qlc_input.add_channel(start=3, width=2) # Diffuse spot
-        self.isi_intensity      = self.qlc_input.add_channel(start=5, width=2) # Master ISI intensity
-        self.spot_color         = self.qlc_input.add_channel(start=7, width=4) # R,G,B,L
-        self.isi_color          = self.qlc_input.add_channel(start=11, width=4) # R,G,B,L
-        self.szene_ctc          = self.qlc_input.add_channel(start=15, width=1) # CTC
-        self.isi_ctc            = self.qlc_input.add_channel(start=16, width=1) # CTC
-        self.qlc_init_channel   = self.qlc_input.add_channel(start=17, width=1) # Init-Button
-        self.sequence_control   = self.qlc_input.add_channel(start=18, width=1) # Control the Sequence of Szene and ISI
-        self.pixel2_7_intensity = self.qlc_input.add_channel(start=19, width=1) # Pixel 2-7 intensity
-        self.reading_light_intensity = self.qlc_input.add_channel(start=20, width=1) # Reading light intensity
-        self.room_light_level   = self.qlc_input.add_channel(start=21, width=1) # Room light
+        self.isi_intensity      = self.qlc_input.add_channel(start=9, width=2) # Master ISI intensity
+        self.spot_color         = self.qlc_input.add_channel(start=11, width=4) # R,G,B,L
+        self.isi_color          = self.qlc_input.add_channel(start=15, width=4) # R,G,B,L
+        self.szene_ctc          = self.qlc_input.add_channel(start=19, width=1) # CTC
+        self.isi_ctc            = self.qlc_input.add_channel(start=20, width=1) # CTC
+        self.qlc_init_channel   = self.qlc_input.add_channel(start=21, width=1) # Init-Button
+        self.sequence_control   = self.qlc_input.add_channel(start=22, width=1) # Control the Sequence of Szene and ISI
+        self.pixel2_7_intensity = self.qlc_input.add_channel(start=23, width=1) # Pixel 2-7 intensity
+        self.reading_light_intensity = self.qlc_input.add_channel(start=24, width=1) # Reading light intensity
+        self.room_light_level   = self.qlc_input.add_channel(start=25, width=1) # Room light
 
     def activate_isi(self):
         self.set_sequence_control("isi")
