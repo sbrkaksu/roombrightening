7329b674aef0edb18ed129b23a140b0c46aef0a4
Berk Aksu <berkaksu79@gmail.com>
2026-07-29 18:56:42 +0200

    max E changed

--- Stage_RaumaufhellungSteuerung.py
+++ Stage_RaumaufhellungSteuerung.py
@@ -131,7 +131,7 @@ class App(ctk.CTk, AsyncCTk):
             "inter-stimulus-interval":  2.5, #2.5, #seconds
             "isi_fade_duration" :       0.5, # seconds. QLC fades in 300 ms
             "sequence_pause_duration":  45.0, # 45 seconds
-            "maxE_spot1": 122,
+            "maxE_spot1": 165,
             "maxE_diffus": 433,
             "direct_center_linear_limit": 101,
             "direct_center_curve": (-3.0235e-09, 0.0020, 0.1933),
